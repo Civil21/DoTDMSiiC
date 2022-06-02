fb32899434bf1aa6578978be8f0f4872fb04a700
shkaanas <nastashkirta@gmail.com>
2022-06-02 14:22:54 +0300

    added sliders

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,8 +4,22 @@ import "channels"
 import "@hotwired/turbo-rails"
 
 Rails.start()
-Turbolinks.start()
+
 ActiveStorage.start()
 
 import "bootstrap"
 import "../stylesheets/application"
+import { auto } from "@popperjs/core"
+
+document.addEventListener('turbo:load', () => {
+    if (document.querySelector('.splide')){
+        let spliders = document.querySelectorAll('.splide');
+        for(let i = 0; i< spliders.length; i++){
+            new Splide( spliders[i],{
+                autowidth: true, 
+                arrows: false,
+                pagination: false
+            } ).mount();
+        }   
+    }
+  } );
